97908e354ffd0ab006a9bd780506e5dabffd34bf
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2026-02-22 15:31:06 -0800

    Reorder front page sections to show family stories after slider

--- front-page.php
+++ front-page.php
@@ -12,11 +12,11 @@ get_header();
 
 <?php get_template_part( 'template-parts/front-slider' ); ?>
 
+<?php get_template_part( 'template-parts/testimonials' ); ?>
+
 <?php get_template_part( 'template-parts/services' ); ?>
 
 <?php get_template_part( 'template-parts/about' ); ?>
-
-<?php get_template_part( 'template-parts/testimonials' ); ?>
 
 <?php get_template_part( 'template-parts/cta' ); ?>
 
